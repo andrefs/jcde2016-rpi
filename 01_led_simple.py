f787d00f0d78014e7ebc32776fea0acabf2f8278
André Santos <andrefs@andrefs.com>
2016-05-13 08:08:39 +0100

    Fixes first step

--- 01_led_simple.py
+++ 01_led_simple.py
@@ -1,5 +1,6 @@
 # Turn LED on
 import RPi.GPIO as GPIO
+from time import sleep
 
 led=7
 
@@ -7,6 +8,7 @@ GPIO.setmode(GPIO.BOARD);
 GPIO.setup(led, GPIO.OUT);
 
 GPIO.output(led,True);
+sleep(10);
 
 GPIO.cleanup()
 
